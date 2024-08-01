136a9da1dc5d2ef6eec0c17200f112d816258ca2
mtinyortaden <mtinyortaden@gmail.com>
2024-08-01 05:24:42 +0000

    terraform week10

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "w7-ts-terraform" #replace with your bucket
+    bucket         = "fhgjhgkjkjjh" #replace with your bucket
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "locktable" #replace with the table
+    dynamodb_table = "vvnnnbvbm" #replace with the table
   }
 }
